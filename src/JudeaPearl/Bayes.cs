8e7439f0c1283045eb94afbad165ae3e2e0a6078
CERobertson <CERobertson@gmail.com>
2015-04-04 16:49:12 -0700

    Initial working environment for Judea Pearl "Probabilisitc Reasoning  in Intelligent Systems:  Networks of Plausable Inference."

---
+++ src/JudeaPearl/Bayes.cs
@@ -0,0 +1,50 @@
+﻿namespace CER.JudeaPearl
+{
+    /// <summary>
+    /// In this attempt at Bayesian formalism, belief measures obey the 
+    /// three basic axioms of probability theory:
+    /// 0 -le P(A) -le 1
+    /// P(Sure proposition) = 1
+    /// P(A or B) = P(A) + P(B) if A and B are mutually exclusive.
+    /// </summary>
+    public static class Bayes
+    {
+        /// <summary>
+        /// Prior odds on Hypothesis
+        /// </summary>
+        /// <param name="pHypothesis">P(H)</param>
+        public static decimal PriorOdds(this decimal pHypothesis)
+        {
+            return pHypothesis / (1 - pHypothesis); 
+        }
+
+        /// <summary>
+        /// Likelihood ratio of evidence given Hypothesis
+        /// </summary>
+        /// <param name="pEvidence_Hypothesis">P(e|H)</param>
+        /// <param name="pEvidence_NoHypothesis">P(e|~H)</param>
+        public static decimal LikelihoodRatio(this decimal pEvidence_Hypothesis, decimal pEvidence_NoHypothesis)
+        {
+            return pEvidence_Hypothesis / pEvidence_NoHypothesis;
+        }
+
+        /// <summary>
+        /// Posterior odds of Hypothesis given evidence
+        /// </summary>
+        /// <param name="lEvidence_Hypothesis">L(e|H)</param>
+        /// <param name="oHypothesis">O(H)</param>
+        public static decimal PosteriorOdds(this decimal lEvidence_Hypothesis, decimal oHypothesis)
+        {
+            return lEvidence_Hypothesis * oHypothesis;
+        }
+
+        /// <summary>
+        /// Probablity of Hypothesis
+        /// </summary>
+        /// <param name="oHypothesis">O(H)</param>
+        public static decimal Probablity(this decimal oHypothesis)
+        {
+            return oHypothesis / (1 + oHypothesis);
+        }
+    }
+}
---
+++ src/Test/priis_Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace CER.Test
+{
+    using CER.JudeaPearl;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Computations of the examples contained in Judea Pearl's book, 
+    /// "Probabilistic Reasoning in Intelligent Systems: Networks of Plausible Inference" 
+    /// 1988 by Morgan Kaufmann Publishers, Inc.
+    /// </summary>
+    [TestClass]
+    public class priis_Tests
+    {
+        [TestMethod]
+        public void Chapter2_Example1()
+        {
+            var pAlarm_Burglary = 0.95M;
+            var pAlarm_NoBurglary = 0.01M;
+            var pBurglary = 0.0001M;
+
+            var lAlarm_Burglary = pAlarm_Burglary.LikelihoodRatio(pAlarm_NoBurglary);
+            var oBurglary = pBurglary.PriorOdds();
+            var oBurglary_Alarm = lAlarm_Burglary.PosteriorOdds(oBurglary);
+            var pBurglary_Alarm = oBurglary_Alarm.Probablity();
+
+            Assert.AreEqual(0.0095M, oBurglary_Alarm);
+            Assert.AreEqual(0.00941M, pBurglary_Alarm);
+        }
+    }
+}
