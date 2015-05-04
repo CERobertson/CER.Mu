cea24a5b1b92c21027f9a7b1038284f157ca732b
CERobertson <CERobertson@gmail.com>
2015-05-04 14:39:03 -0700

    Union a bunch of entity framework collection properties into an enumerable of their shared base class. Seems good for the moment.

--- src/Rpg/DbContext.cs
+++ src/Rpg/DbContext.cs
@@ -5,73 +5,65 @@
     using ef = System.Data.Entity;
     using System.Data.Entity.ModelConfiguration.Conventions;
     using System;
+    using System.Linq;
 
     public class DbContext : ef.DbContext
     {
         public ef.DbSet<game> Games { get; set; }
-
+        public ef.DbSet<player> Players { get; set; }
+        public ef.DbSet<character> Characters { get; set; }
+        public ef.DbSet<role> Roles { get; set; }
+        public ef.DbSet<performance> Performances { get; set; }
+        public ef.DbSet<plot> Plots { get; set; }
+        public ef.DbSet<relationship> Relationships { get; set; }
+        public ef.DbSet<location> Locations { get; set; }
+        
         protected override void OnModelCreating(ef.DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
+        public IEnumerable<element> Elements()
+        {
+            return this.Games.ToList<element>().Union(
+                this.Players.ToList<element>()).Union(
+                this.Characters.ToList<element>()).Union(
+                this.Roles.ToList<element>()).Union(
+                this.Performances.ToList<element>()).Union(
+                this.Plots.ToList<element>()).Union(
+                this.Relationships.ToList<element>()).Union(
+                this.Locations.ToList<element>());
+        }
     }
-    public class game
+
+    public class game : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public string description { get; set; }
         public int current_chapter { get; set; }
         public virtual List<plot> chapters { get; set; }
         public virtual List<performance> performances { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
     }
-    public class player
+    public class player : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
         public string name { get; set; }
-        public string description { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
     }
-    public class character
+    public class character : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public string description { get; set; }
         public virtual List<relationship> relationships { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
     }
-    public class role
+    public class role : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public string description { get; set; }
         public virtual player player { get; set; }
         public virtual character character { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
 
     }
-    public class performance
+    public class performance : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public string description { get; set; }
         public string start { get; set; }
         public decimal duration { get; set; }
         public virtual role role { get; set; }
     }
-    public class plot
+    public class plot : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public string description { get; set; }
         public string introduction { get; set; }
         public decimal estimated_duration { get; set; }
         public int scale_duration { get; set; }
@@ -80,25 +72,12 @@
         public virtual List<character> characters { get; set; }
         public virtual List<relationship> relationships { get; set; }
         public virtual List<performance> participants { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
     }
-    public class relationship
+    public class relationship : element
     {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public string description { get; set; }
         public int priority { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
-
     }
-    public class location
-    {
-        [Key]
-        public int id { get; set; }
-        public string gm_name { get; set; }
-        public virtual List<creation_process> creation_history { get; set; }
-    }
+    public class location : element { }
     public class creation_process
     {
         [Key]
@@ -106,6 +85,14 @@
         public string title { get; set; }
         public string description { get; set; }
         public string video { get; set; }
+    }    
+    public abstract class element
+    {
+        [Key]
+        public int id { get; set; }
+        public string gm_name { get; set; }
+        public string description { get; set; }
+        public virtual List<creation_process> creation_history { get; set; }
     }
 }
 
---
+++ src/Test/EntityTests.cs
@@ -0,0 +1,18 @@
+﻿namespace CER.Test
+{
+    using CER.Rpg;
+    using System;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EntityTests
+    {
+        [TestMethod]
+        public void Rpg()
+        {
+            var rpg = new DbContext();
+            rpg.Elements().Count(); 
+        }
+    }
+}
