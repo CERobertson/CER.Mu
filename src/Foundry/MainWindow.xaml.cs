ea8f885facaa946284aef45573b00396400dd4c0
CERobertson <CERobertson@gmail.com>
2015-05-06 18:04:30 -0700

    allowed vs code gen to run on this WPF app to see how it wants to setup data binding in xaml.  The results seem close to minimum.  Time to make app functions.

--- src/Foundry/MainWindow.xaml.cs
+++ src/Foundry/MainWindow.xaml.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace CER.Foundry
+﻿namespace CER.Foundry
 {
+    using CER.Mu;
+    using CER.Rpg;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DbContext rpg = new DbContext(new CreateSeedDatabaseIfNotExists());
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Data.CollectionViewSource gameViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("gameViewSource")));
+            this.rpg.Games.ToList();
+            gameViewSource.Source = this.rpg.Games.Local;
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            this.rpg.Dispose();
         }
     }
 }
