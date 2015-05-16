fca51e4ccdeb14a0e915643103667863660c5c7a
CERobertson <CERobertson@gmail.com>
2015-05-16 14:48:24 -0700

    pulling over most (all?) of the differences from wpf branch.

---
+++ src/Foundry/Game.xaml.cs
@@ -0,0 +1,40 @@
+﻿using CER.Mu;
+using CER.Rpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CER.Foundry
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game : Page
+    {
+        public static DependencyProperty ToObserveProperty = DependencyProperty.Register("ToObserve", typeof(game), typeof(Game));
+
+        public game ToObserve
+        {
+            get { return (game)this.GetValue(Game.ToObserveProperty); }
+            set { this.SetValue(Game.ToObserveProperty, value); }
+        }
+
+        private DbContext rpg = new DbContext(new CreateSeedDatabaseIfNotExists());
+
+        public Game()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- src/Foundry/MainWindow.xaml.cs
+++ src/Foundry/MainWindow.xaml.cs
@@ -2,20 +2,13 @@
 {
     using CER.Mu;
     using CER.Rpg;
-    using System;
-    using System.Collections.Generic;
+    using Microsoft.Win32;
+    using System.ComponentModel;
+    using System.IO;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using System.Windows;
-    using System.Windows.Controls;
     using System.Windows.Data;
     using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
 
 
     /// <summary>
@@ -23,7 +16,16 @@
     /// </summary>
     public partial class MainWindow : Window
     {
+        public readonly string DialogFilter = "XAML Files (*.xaml)|*.xaml|RichText Files (*.rtf)|*.rtf|All files (*.*)|*.*";
         private DbContext rpg = new DbContext(new CreateSeedDatabaseIfNotExists());
+
+        public static DependencyProperty InternalDocument_PreviousPartitionHeight_Property = DependencyProperty.Register("InternalDocument_PreviousPartitionHeight", typeof(int), typeof(MainWindow));
+
+        public int InternalDocument_PreviousPartitionHeight
+        {
+            get { return (int)this.GetValue(MainWindow.InternalDocument_PreviousPartitionHeight_Property); }
+            set { this.SetValue(MainWindow.InternalDocument_PreviousPartitionHeight_Property, value); }
+        }
 
         public MainWindow()
         {
@@ -32,15 +34,113 @@
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            System.Windows.Data.CollectionViewSource gameViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("gameViewSource")));
+            //hooks up the collection viewer, most likely wont last
+            var gameViewSource = (CollectionViewSource)this.FindResource("gameViewSource");
             this.rpg.Games.ToList();
             gameViewSource.Source = this.rpg.Games.Local;
+
+            //hooks up a navigation frame to a default page.
+            this.NavigateRpgFrameToGame("mu");
+
+            //records the initial height of the InternalDocument_Partition grid splitter.
+
         }
 
-        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        protected override void OnClosing(CancelEventArgs e)
         {
             base.OnClosing(e);
+            this.rpg.SaveChanges();
             this.rpg.Dispose();
+        }
+
+        private void NewCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            this.Editor.Document = new FlowDocument();
+        }
+
+        private void OpenCommand_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = this.DialogFilter;
+
+            if (dialog.ShowDialog() == true)
+            {
+                var textRange = new TextRange(
+                    this.Editor.Document.ContentStart,
+                    this.Editor.Document.ContentEnd);
+                using (var fs = File.Open(dialog.FileName, FileMode.Open))
+                {
+                    if (Path.GetExtension(dialog.FileName).ToLower() == ".rtf")
+                    {
+                        textRange.Load(fs, DataFormats.Rtf);
+                    }
+                    else
+                    {
+                        textRange.Load(fs, DataFormats.Xaml);
+                    }
+                }
+            }
+        }
+
+        private void SaveBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = this.DialogFilter;
+
+            if (dialog.ShowDialog() == true)
+            {
+                var textRange = new TextRange(
+                    this.Editor.Document.ContentStart,
+                    this.Editor.Document.ContentEnd);
+                using (var fs = File.Create(dialog.FileName))
+                {
+                    if (Path.GetExtension(dialog.FileName).ToLower() == ".rtf")
+                    {
+                        textRange.Save(fs, DataFormats.Rtf);
+                    }
+                    else
+                    {
+                        textRange.Save(fs, DataFormats.Xaml);
+                    }
+                }
+            }
+        }
+
+        private void Editor_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            using (var s = new MemoryStream())
+            {
+                var textRange = new TextRange(
+                    this.Editor.Document.ContentStart,
+                    this.Editor.Document.ContentEnd);
+                textRange.Save(s, DataFormats.Xaml);
+                s.Position = 0;
+
+                using (var r = new StreamReader(s))
+                {
+                    this.EditorMarkup.Text = r.ReadToEnd();
+                }
+            }
+        }
+        
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            var link = (Hyperlink)sender;
+            var text = new TextRange(link.ContentStart, link.ContentEnd).Text;
+            var prototype = new game { gm_name = text };
+            this.NavigateRpgFrameToGame(text, prototype);
+        }
+
+        private void NavigateRpgFrameToGame(string key, game obj = null)
+        {
+            var game = new Game();
+            game.ToObserve = this.rpg.SingleOrCreate(this.rpg.Games, x => x.gm_name == key, obj);
+            this.RpgFrame.Navigate(game);
+        }
+        
+        private void GridSplitter_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var current_height = this.InternalDocument_Partition.Height;
         }
     }
 }
