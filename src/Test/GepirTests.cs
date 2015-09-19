4ce74df94b6f8fba9fee736421bea75ee45ab402
CERobertson <CERobertson@gmail.com>
2015-09-19 16:15:43 -0700

    Added a web reference to GEPIR. The request returns a null value.  Probably something to do with how I am creating the request, for example I guessed on the language and the version number is a best guess from looking around online.  This was a bit of a lark, therefore good enough for now.

---
+++ src/Test/GepirTests.cs
@@ -0,0 +1,17 @@
+﻿namespace CER.Test
+{
+    using CER.Test.org.gs1us.prod.gepir;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class GepirTests
+    {
+        [TestMethod]
+        public void GetInfo()
+        {
+            CER.Test.org.gs1us.prod.gepir.router r = new router();
+            var o = r.GetItemByGTIN(new GetItemByGTIN { requestedGtin = "0788687", requestedLanguages = new string[] { "En-US" }, version = 4.0M });
+        }
+    }
+}
---
+++ src/Test/Web References/org.gs1us.prod.gepir/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CER.Test.org.gs1us.prod.gepir
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
