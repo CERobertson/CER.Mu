6b786ff7b63aa50de7388de0d0211e3c99d8530d
CERobertson <CERobertson@gmail.com>
2015-09-26 14:01:05 -0700

    Work in progress.

--- src/Test/CompilerTests.cs
+++ src/Test/CompilerTests.cs
@@ -5,6 +5,7 @@
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System.Text.RegularExpressions;
     using CER.Text;
+    using System.Collections.Generic;
 
     [TestClass]
     public class CompilerTests
@@ -25,6 +26,27 @@
             compilerToTest.Regex[Tokens.Number] = @"\d+";
             scan_results = compilerToTest.Scan(test).ToList();
             Assert.AreEqual(scan_results.Count, 5);
+        }
+        [TestMethod]
+        public void CalcExp()
+        {
+            this.print(Compiler.CalculateExponents(0, 155));
+            this.print(Compiler.CalculateExponents(1, 155));
+            this.print(Compiler.CalculateExponents(123, 155));
+            this.print(Compiler.CalculateExponents(154, 155));
+            this.print(Compiler.CalculateExponents(155, 155));
+            this.print(Compiler.CalculateExponents(156, 155));
+            this.print(Compiler.CalculateExponents(3723875, 155));
+            this.print(Compiler.CalculateExponents(3851372, 155));
+            this.print(Compiler.CalculateExponents(3723874, 155));
+        }
+        private void print(IEnumerable<int> a)
+        {
+            foreach(int i in a)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
         }
     }
     public enum Tokens
--- src/Text/Compiler.cs
+++ src/Text/Compiler.cs
@@ -2,11 +2,76 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text.RegularExpressions;
+    using a = Microsoft.Office.Interop.Excel;
 
     public class Compiler
     {
+        public static IEnumerable<string> EnumerateOrdinals(int limit, string input = @"C:\Users\Cory\SkyDrive\Ordinal.txt", string output = @"C:\Users\Cory\SkyDrive\Ordinal.xlsx")
+        {
+            string[] base_enuneration;
+            using (TextReader reader = File.OpenText(input))
+            {
+                base_enuneration = reader.ReadToEnd().Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            }
+            var width = base_enuneration.Length;
+            var exponents = Compiler.CalculateExponents(limit, width);
+            while (limit > 0)
+            {
+                yield return string.Format("{0},{1}", limit, Compiler.CreateNumberGlyph(base_enuneration, exponents));
+                limit--;
+            }
+        }
+
+        private static string CreateNumberGlyph(string[] enumeration, int[] exponents)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < exponents.Length; i++)
+            {
+                if (exponents[i] == 0)
+                {
+                    result += "0";
+                }
+                else
+                {
+                    result += enumeration[exponents[i-1]];
+                }
+            }
+            return result;
+        }
+        public static int[] SubtractOneFromExponents(int[] exponents, int depth=0)
+        {
+            for (int i =0; i< exponents.Length; i++)
+            {
+
+            }
+            return exponents;
+        }
+
+        public static int[] CalculateExponents(int x, int b)
+        {
+            int[] exponents;
+            Compiler.calc_exp(x, b, 0, out exponents);
+            return exponents;
+        }
+        public static void calc_exp(int x, int b, int depth, out int[] exponents)
+        {
+            depth++;
+            var exponent = x / b;
+            if (exponent != 0)
+            {
+                Compiler.calc_exp(exponent, b, depth, out exponents);
+            }
+            else
+            {
+                exponents = new int[depth];
+            }
+            var index = exponents.Length - depth;
+            exponents[index] = x % b;
+        }
+
         public Compiler()
         {
             this.Regex = new Dictionary<object, string>();
