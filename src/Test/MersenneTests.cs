6773fc572186969872b507210df4ec95b487dc1d
CERobertson <CERobertson@gmail.com>
2017-03-03 22:02:51 -0800

    udp test and a mersenne prime test without a library to test.

---
+++ src/Test/MersenneTests.cs
@@ -0,0 +1,27 @@
+﻿using CER.Wikipedia;
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CER.Test {
+    /// <summary>
+    /// Summary description for MersenneTests
+    /// </summary>
+    [TestClass]
+    public class MersenneTests {
+        [TestMethod]
+        public void ExploreNumbers() {
+            int primes_found = 0;
+            int length = 8;
+            int[][] merseen_primes = new int[length][];
+            for (int i = 3; i < length; i++) {
+                int M = Mersenne.Number(i);
+                if (Mersenne.Lucas_LehmerPrimalityTest(i, M)) {
+                    merseen_primes[primes_found] = new[] { i, M };
+                    primes_found++;
+                }
+            }
+        }
+    }
+}
---
+++ src/Test/UDPTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace CER.Test {
+    [TestClass]
+    public class UDPTests {
+        [TestMethod]
+        public void UDPClient() {
+            //Creates a UdpClient for reading incoming data.
+            UdpClient receivingUdpClient = new UdpClient(52138);
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender. 
+            // The IPEndPoint will allow you to read datagrams sent from any source.
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Byte[] receiveBytes = receivingUdpClient.Receive(ref RemoteIpEndPoint);
+
+            string returnData = Encoding.ASCII.GetString(receiveBytes);
+        }
+    }
+}
